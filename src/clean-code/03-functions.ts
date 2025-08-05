9052aaed3e9044bfd5512fc022df4f53da69382c
Juan <ja.morenofernandez@gmail.com>
2025-08-05 22:26:03 +0200

    functions, homework

---
+++ src/clean-code/03-functions.ts
@@ -0,0 +1,48 @@
+(() => {
+
+    // función para obtener información de una película por Id
+    function getMovieById(id: string) {
+        console.log({ id });
+    }
+
+    // función para obtener información de los actores de una película - Actors o Cast // id = movieId getMovieCast
+    function getMovieCastById(id: string) {
+        console.log({ id });
+    }
+
+    // funcion para obtener el bio del actor por el id
+    function getActorBioById(id: string) {
+        console.log({ id });
+    }
+
+    interface Movie {
+        cast: string[]
+        description: string;
+        rating: number;
+        title: string;
+    }
+
+    // Crear una película
+    function createMovie({ cast, description, rating, title }: Movie) {
+        console.log({ title, description, rating, cast });
+    }
+
+    // Crea un nuevo actor
+    function createActor(fullName: string, birthdate: Date): boolean {
+
+        // tarea asincrona para verificar nombre
+        // ..
+        // ..
+        if (fullName === 'fernando') return false;
+
+        console.log('Crear actor');
+        return true;
+
+    }
+
+    const getPayAmount = ({ isDead = false, isSeparated = true, isRetired = false }):number => {
+        return isDead ? 1500 : isSeparated ? 2500 : isRetired ? 3000 : 4000;        
+    }
+
+
+})();
